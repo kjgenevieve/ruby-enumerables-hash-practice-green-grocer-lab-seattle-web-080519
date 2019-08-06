418a14e072624cd7d73ffae997b01bbe4d15ff54
Genevieve McAllister <kj.genevieve@gmail.com>
2019-08-06 00:10:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -71,6 +71,7 @@ def consolidate_cart(cart)
   consolidated_items = {}
   cart.each do |item|
     if consolidated_items[item.keys[0]]
+      binding.pry
       consolidated_items[item.keys[0]][:count] += 1
     else
         consolidated_items[item.keys[0]] = {
